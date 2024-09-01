82dbab1e9ec973a798c9e380869b24b9a58240a5
Gian Mateus <gian.m.silver@gmail.com>
2024-09-01 11:10:55 -0300

    fix: transition navbar

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -12,7 +12,7 @@ export default function _Header() {
 
   return (
     <header className="sticky top-0 z-10 flex min-w-full justify-center bg-primary text-background">
-      <nav className="flex w-full max-w-screen-lg justify-between px-12 py-4 font-bold items-center">
+      <nav className="flex w-full max-w-screen-lg items-center justify-between overflow-hidden px-12 py-4 font-bold">
         <div>
           <a href="/" className="flex items-center gap-x-4">
             <LogoIcon fill="#fff" opacity="1" width="45" height="45" />
@@ -32,20 +32,36 @@ export default function _Header() {
         </button>
         <ul
           className={`${
-            isMenuOpen ? "flex" : "hidden"
-          } absolute left-0 top-full w-full gap-4 bg-primary p-4 text-xl font-normal md:static md:flex md:items-center text-center flex-col md:flex-row justify-end transition-all`}
+            isMenuOpen ? "h-[200px]" : "h-0 p-0"
+          } absolute left-0 top-full box-border flex w-full flex-col items-center justify-end gap-4 overflow-hidden bg-primary p-4 text-center text-xl font-normal transition-all md:static md:flex md:h-auto md:flex-row md:items-center`}
         >
           <li>
-            <a href="#">Sobre</a>
+            <a
+              className="relative block w-fit after:absolute after:block after:h-[3px] after:w-full after:origin-left after:scale-x-0 after:bg-background after:transition after:duration-300 after:content-[''] after:hover:scale-x-100"
+              href="#">
+              Sobre
+            </a>
           </li>
           <li>
-            <a href="#">Soluções</a>
+            <a
+              className="relative block w-fit after:absolute after:block after:h-[3px] after:w-full after:origin-left after:scale-x-0 after:bg-background after:transition after:duration-300 after:content-[''] after:hover:scale-x-100"
+              href="#">
+              Soluções
+            </a>
           </li>
           <li>
-            <a href="#">Palestras</a>
+            <a
+              className="relative block w-fit after:absolute after:block after:h-[3px] after:w-full after:origin-left after:scale-x-0 after:bg-background after:transition after:duration-300 after:content-[''] after:hover:scale-x-100"
+              href="#">
+              Palestras
+            </a>
           </li>
           <li>
-            <a href="#">Contatos</a>
+            <a
+              className="relative block w-fit after:absolute after:block after:h-[3px] after:w-full after:origin-left after:scale-x-0 after:bg-background after:transition after:duration-300 after:content-[''] after:hover:scale-x-100"
+              href="#">
+              Contatos
+            </a>
           </li>
         </ul>
       </nav>
